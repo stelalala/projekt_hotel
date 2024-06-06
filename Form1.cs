371eaba6f7ddf8656548dcab2e6a985aa3781e69
lucejericevic <luce.jericevic@skole.hr>
2024-06-06 12:22:31 +0200

    Odradjena Form1 u kojoj se prijavis. Spojena s Form2 i Form3

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,68 @@ namespace projekt_hotel
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                label10.Visible = true;
+                label10.Text = "Unesite korisničko ime!";
+            }
+            else if (textBox2.Text == "")
+            {
+                label10.Visible = true;
+                label10.Text = "Unesite lozinku!";
+            }
+            else if (textBox5.Text == "")
+            {
+                label10.Visible = true;
+                label10.Text = "Unesite svoje ime!";
+            }
+            else if (textBox6.Text == "")
+            {
+                label10.Visible = true;
+                label10.Text = "Unesite svoje prezime!";
+            }
+            else
+            {
+                this.Hide();
+                Form2 form2 = new Form2();
+                form2.ShowDialog();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (textBox3.Text == "")
+            {
+                label11.Visible = true;
+                label11.Text = "Unesite korisničko ime!";
+            }
+            else if (textBox4.Text == "")
+            {
+                label11.Visible = true;
+                label11.Text = "Unesite lozinku!";
+            }
+            else if ((textBox3.Text == "josipa.loncar" && textBox4.Text == "josipa1234") || (textBox3.Text == "stela.pulic" && textBox4.Text == "stela1234"))
+            {
+                this.Hide();
+                Form3 form3 = new Form3();
+                form3.ShowDialog();
+            }
+            else
+            {
+                label11.Visible = true;
+                label11.Text = "Neispravno korisničko ime ili lozinka!";
+            }
+        }
+
     }
 }
